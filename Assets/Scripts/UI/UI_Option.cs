00b8701f3ec8e19d88e7da026d9793681ddf1c11
LeeJunho <tjdi146534@gmail.com>
2024-06-18 17:03:00 +0900

    Option 진행2

--- Assets/Scripts/UI/UI_Option.cs
+++ Assets/Scripts/UI/UI_Option.cs
@@ -5,21 +5,7 @@ using UnityEngine.UI;
 
 public class UI_Option : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        CanvasScaler canvasScaler = GetComponent<CanvasScaler>();
-        if (canvasScaler != null)
-        {
-            canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
-            canvasScaler.referenceResolution = new Vector2(1920, 1080);
-            canvasScaler.screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
-            canvasScaler.matchWidthOrHeight = 0.5f;  // ±ÕÇü ¸ÂÃß±â
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
         
     }
